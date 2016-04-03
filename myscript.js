3f8ad3e0f19fcd483e3b937691b764801ed9cd78
Catherine Shannon <irishgal173@yahoo.com>
2016-04-03 11:43:19 +0200

    more content - right before adding the responsive menu

--- myscript.js
+++ myscript.js
@@ -1,19 +1,51 @@
-// $("#toTop").click(function () {
-//    $("html, body").animate({scrollTop: 0}, 1000);
-// });
 
 
- // $(document).ready(function() {
 
- // $('#toTop').click = function (){
- //  $(document).scrollTo('#home');
- // } 
-
- // });
-
- $('#toTop').click(function() {
+$('#toTop').click(function() {
     $('html,body').animate({
-        scrollTop : $('#top').offset().top
+        scrollTop : $('#home').offset().top
+    	},500);
+});
+
+$('#contact-bttn').click(function() {
+    $('html,body').animate({
+        scrollTop : $('#contact').offset().top
+    	},500);
+});
+
+$('#home-bttn').click(function() {
+    $('html,body').animate({
+        scrollTop : $('#home').offset().top
+    	},500);
+});
+
+$('#events-bttn').click(function() {
+    $('html,body').animate({
+        scrollTop : $('#events').offset().top
+    	},500);
+});
+
+$('#menu-bttn').click(function() {
+    $('html,body').animate({
+        scrollTop : $('#menu').offset().top
+    	},500);
+});
+
+$('#about-bttn').click(function() {
+    $('html,body').animate({
+        scrollTop : $('#about').offset().top
+    	},500);
+});
+
+$('#faq-bttn').click(function() {
+    $('html,body').animate({
+        scrollTop : $('#faq').offset().top
+    	},500);
+});
+
+$('#photos-bttn').click(function() {
+    $('html,body').animate({
+        scrollTop : $('#photos').offset().top
     	},500);
 });
 
